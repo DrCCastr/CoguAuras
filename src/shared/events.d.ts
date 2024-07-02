88abe375e6151b85c4213f7d1d212b5c7b581ecc
DrCCastr <enzinhocastroo13@gmail.com>
2024-07-02 19:52:55 -0300

    add events module

---
+++ src/shared/events.d.ts
@@ -0,0 +1,24 @@
+type Tuple<T = unknown> = [T] | [undefined, string];
+
+type Player = Instance & {
+	Name: string;
+};
+
+declare namespace SafeFunctions {
+	interface Server {
+		BindToInvokeServer(Name: string, func: (Player: Player, ...args: unknown[]) => unknown): void;
+		UnbindInvokeServer(Name: string): void;
+		StopInvokeServer(Name: string): void;
+		InvokeClient(Player: Player, Name: string, Timeout: number, ...args: unknown[]): Tuple;
+	}
+
+	interface Client {
+		BindToInvokeClient(Name: string, func: (...args: unknown[]) => unknown): void;
+		UnbindInvokeClient(Name: string): void;
+		StopInvokeClient(Player: Player, Name: string): void;
+		InvokeServer(Name: string, Timeout: number, ...args: unknown[]): Tuple;
+	}
+}
+
+declare const SafeFunctions: SafeFunctions.Server & SafeFunctions.Client;
+export = SafeFunctions;
